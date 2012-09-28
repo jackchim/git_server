e6776b40d4848a65a3da926a8f81447089e921f8
Jack.Chim <ziikii1@qq.com>
2012-09-28 18:47:27 +0800

    blabla 未完

--- git_server_install.sh
+++ git_server_install.sh
@@ -1,2 +1,15 @@
 #!/bin/bash 
 yum install -y git-core
+#新加用户git，该用户将作为所有代码仓库和用户权限的管理者
+useradd -m git 
+
+#为git设置密码 
+password git 
+
+#建立一个git仓库的存储点
+mkdir /opt/git 
+
+chown git:git /opt/git 
+chmod 700 /opt/git 
+
+
